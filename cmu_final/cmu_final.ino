c67762e500aaff580fd3876df85b4cc01bf2e4db
beyondszine <beyond.hack@gmail.com>
2015-04-28 21:00:31 -0400

    cmu adc_file working

--- cmu_final/cmu_final.ino
+++ cmu_final/cmu_final.ino
@@ -7,15 +7,23 @@ int fan_relay=4;
 
 
 //These five load relays are Active LOW------
-int load_resistor_5=12;
-int load_resistor_10=9;
-int load_resistor_20=10;
-int load_resistor_40=8;
-int load_resistor_80=11;
+int load_resistor_5ohm   = 12;
+int load_resistor_10ohm  = 9;
+int load_resistor_20ohm  =10;
+int load_resistor_40ohm  =8;
+int load_resistor_80ohm  =11;
 //-------------------------------------------
-unsigned int net_adc_current,net_adc_panel,net_adc_bat;
+unsigned int net_adc_panel,net_adc_bat,net_adc_load;
+float net_adc_current;
+float current;
 int average_no=10;
 float voltage_coff=15.0/1024.0;
+
+unsigned int adc_bat;
+unsigned int adc_panel;
+unsigned int adc_load;
+unsigned int adc_current;
+
 
 
 int i=0;
@@ -28,11 +36,11 @@ void setup()
         pinMode(src_select_solar_relay,OUTPUT);
         pinMode(fan_relay,OUTPUT);
 
-        pinMode(load_resistor_80,OUTPUT);
-        pinMode(load_resistor_40,OUTPUT);
-        pinMode(load_resistor_20,OUTPUT);
-        pinMode(load_resistor_10,OUTPUT);
-        pinMode(load_resistor_5,OUTPUT);
+        pinMode(load_resistor_80ohm,OUTPUT);
+        pinMode(load_resistor_40ohm,OUTPUT);
+        pinMode(load_resistor_20ohm,OUTPUT);
+        pinMode(load_resistor_10ohm,OUTPUT);
+        pinMode(load_resistor_5ohm,OUTPUT);
         
         
         digitalWrite(src_select_battery_relay,LOW);
@@ -40,37 +48,51 @@ void setup()
         digitalWrite(fan_relay,HIGH);
         digitalWrite(src_select_solar_relay,LOW);
 
-        digitalWrite(load_resistor_5,HIGH);
-        digitalWrite(load_resistor_10,HIGH);
-        digitalWrite(load_resistor_20,HIGH);
-        digitalWrite(load_resistor_40,HIGH);
-        digitalWrite(load_resistor_80,LOW);
+        digitalWrite(load_resistor_5ohm,LOW);
+        digitalWrite(load_resistor_10ohm,LOW);
+        digitalWrite(load_resistor_20ohm,LOW);
+        digitalWrite(load_resistor_40ohm,LOW);
+        digitalWrite(load_resistor_80ohm,HIGH);
         pinMode(13,OUTPUT);
         Serial.begin(9600);      
 }
 
 void loop()
 {
-  digitalWrite(13,HIGH);
+ // digitalWrite(13,HIGH);
   for (i=0;i<average_no;i++)
   { 
-    unsigned int adc_bat = analogRead(A0);
-    unsigned int adc_panel = analogRead(A1);
-    unsigned int adc_current = analogRead(A2);
+    adc_current = analogRead(A4);
+    current = adc_current * (5.0 / 1023.0);
+    adc_bat = analogRead(A0);
+    delay(10);
+    adc_panel = analogRead(A1);
+    delay(10);
+    adc_load = analogRead(A2);
+    delay(10);
     
     net_adc_bat     += adc_bat;
     net_adc_panel   += adc_panel;
-    net_adc_current += adc_current;
+    net_adc_load    += adc_load;
+    net_adc_current += current;
     
   }
-   net_adc_bat  /=  average_no;
-   net_adc_panel  /=  average_no;
+   net_adc_bat      /=  average_no;
+   net_adc_panel    /=  average_no;
+   net_adc_load     /=  average_no;
    net_adc_current  /=  average_no;
    
    
   // print out the value you read:
+
   Serial.print("Voltage_battery --");
   Serial.print(voltage_coff*net_adc_bat);
+  Serial.print("\t\t");
+  Serial.print("Voltage_Panel --");
+  Serial.print(voltage_coff*net_adc_panel);
+  Serial.print("\t\t");
+  Serial.print("Voltage_LOAD --");
+  Serial.print(voltage_coff*net_adc_load);
   Serial.print("\t\t");
   Serial.print("Current --");
   Serial.println(net_adc_current);
